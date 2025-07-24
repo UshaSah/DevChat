d05486fe14965578bb58b18e7f71e6fed1e91c9b
UshaSah <usah@ucdavis.edu>
2025-07-23 17:27:46 -0700

    added middleware to verify webtoken

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require("../../models/User");
 
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
-router.get('/', (req, res) => {
+router.get('/', auth, async (req, res) => {
     try {
-        res.json({ message: 'Auth route working' });
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Server Error');
+        res.status(500).send('Server error');
     }
-});
 
+});
 module.exports = router;
